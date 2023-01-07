85332302f47dddee8cb803ef42e75b1c800e1b9a
ahmed iblao <ahmediblao532gmail.com>
2023-01-08 01:48:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,119 @@
+function navSlide() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  
+  burger.addEventListener("click", () => {
+      //Toggle Nav
+      nav.classList.toggle("nav-active");
+      
+      //Animate Links
+      navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+              link.style.animation = ""
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+          }
+      });
+      //Burger Animation
+      burger.classList.toggle("toggle");
+  });
+  
+}
+
+navSlide();
+
+
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+
+  console.log(Math.floor(window.innerWidth / 270));
+});
+
+
+function reload(){
+  location.reload();
+}
+
+function login(){
+  location.href="/login.html"
+}
+
+function logout(){
+  location.href="/index.html"
+}
+
+function trnds(){
+  scroll({
+    left:0,
+    top:300,
+    behavior:'smooth'
+    
+})
+}
+
+function movie(){
+  location.href="main.html"
+  scroll({
+    left:0,
+    top:920,
+    behavior:'smooth'
+})
+
+}
+
+function Popular(){
+  scroll({
+    left:0,
+    top:2250,
+    behavior:'smooth'
+    
+})
+}
+
+
+function tvshows(){
+  location.href="tvshows.html"
+}
+
+
+
+let send = document.querySelector('#send')
+
+send.onclick= function(){
+  alert("you have sent your message succfuly")
+}
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("goUpBtn").style.display = "block";
+  } else {
+    document.getElementById("goUpBtn").style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
